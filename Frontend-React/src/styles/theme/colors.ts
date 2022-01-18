4c42a480310346deb08b63e071f5032352a41e5b
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2022-01-18 16:03:17 -0300

    First Commit BoilerPlates - You are free to make any changes

---
+++ Frontend-React/src/styles/theme/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+    PRIMARY: "#75D6F4",
+    PRIMAREYTEXT: "#96CDFF",
+    PRIMARYDARKER: "#2B373A",
+    BACKGROUND: "radial-gradient(68.98% 68.98% at 50% 13.36%, #75D6F4 2.3%, #010101 100%)",
+}
---
+++ Frontend-React/src/styles/theme/index.ts
@@ -0,0 +1,12 @@
+import { colors } from "./colors";
+
+const theme = {
+    color:{
+        primary:colors.PRIMARY,
+        primaryText:colors.PRIMAREYTEXT,
+        primaryDarker:colors.PRIMARYDARKER,
+        backgroundApp:colors.BACKGROUND
+    }
+}
+
+export default theme
